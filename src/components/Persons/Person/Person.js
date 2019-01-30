c23bcef06877b743ff4bb8adf218e8ce851c9641
JaaKto <33008680+JaaKto@users.noreply.github.com>
2019-01-30 21:24:16 +0100

    added second hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   constructor(props) {
@@ -19,13 +21,13 @@ class Person extends Component {
     console.log('[Person.js] indise render()')
 
     return (
-      <div className={classes.Person}>
+      <Auxiliary>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
         <p>{this.props.children}</p>
         <input type="" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Auxiliary>
     )
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
